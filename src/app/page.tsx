ce78b40d46fa2a6c91f9c937d772be703ad7bfbc
Jieun Han <78632052+onezeun@users.noreply.github.com>
2024-12-03 18:34:41 +0900

    배포오류수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
 import dynamic from 'next/dynamic';
 
 const ChristmasEnvelope = dynamic(() => import('../components/ChristmasEnvelope'), { ssr: false });
 
 export default function Page() {
-    return <ChristmasEnvelope />;
+  return <ChristmasEnvelope />;
 }
